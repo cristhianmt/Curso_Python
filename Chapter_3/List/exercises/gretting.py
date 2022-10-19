d43fe96a9d04a0a3d229fe1eff93372ead6eab46
Cristhian <cmartinezg1706@alumno.ipn.mx>
2022-10-18 20:00:34 -0500

    exercises about list

---
+++ Chapter_3/List/exercises/gretting.py
@@ -0,0 +1,12 @@
+"""
+3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+printing each person’s name, print a message to them. The text of each message
+should be the same, but each message should be personalized with the
+person’s name.
+"""
+
+name_friends = ['bob','alice','tom','kelly']
+print(f"Hi my name is {name_friends[0]}")
+print(f"Hi my name is {name_friends[1]}")
+print(f"Hi my name is {name_friends[2]}")
+print(f"Hi my name is {name_friends[3]}")
---
+++ Chapter_3/List/exercises/your_own_list.py
@@ -0,0 +1,14 @@
+"""
+3-3. Your Own List: Think of your favorite mode of transportation, such as a
+motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a
+Honda motorcycle.”
+"""
+
+transport = ['car', 'private airplane', 'skate','bus','subway']
+
+print(f"I would like to own a {transport[0]}")
+print(f"I would like to fly in a {transport[1]}")
+print(f"I would like to learn {transport[2]}")
+print(f"I would like to drive a {transport[3]} and {transport[4]}")
+
