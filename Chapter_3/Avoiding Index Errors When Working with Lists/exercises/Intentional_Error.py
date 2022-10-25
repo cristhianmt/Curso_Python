f98e413ddd19628bcbaa921dee068c2b4986505d
Cristhian <cmartinezg1706@alumno.ipn.mx>
2022-10-25 18:01:55 -0500

    Index Errors

---
+++ Chapter_3/Avoiding Index Errors When Working with Lists/exercises/Intentional_Error.py
@@ -0,0 +1,10 @@
+"""
+3-11. Intentional Error: If you haven’t received an index error in one of your programs
+yet, try to make one happen. Change an index in one of your programs
+to produce an index error. Make sure you correct the error before closing the
+program.
+"""
+
+linux_os = ['ubuntu', 'fedora', 'debian', 'mint', 'zorin', 'kali', 'arch']
+print(linux_os[7])
+
---
+++ Chapter_3/Avoiding Index Errors When Working with Lists/motorcycles.py
@@ -0,0 +1,14 @@
+"""
+There’s one type of error that’s common to see when you’re working with
+lists for the first time. Let’s say you have a list with three items, and you ask
+for the fourth item:
+"""
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles[3])
+print(motorcycles[-1])
+
+motorcycles = []
+print(motorcycles[])
+
+
